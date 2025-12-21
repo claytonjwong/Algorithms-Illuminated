0296627934e3ad209f0ddd6113ce552842735a1f
claytonjwong <claytonjwong@gmail.com>
2025-12-21 08:44:25 -0700

    recursive integer multiplication (Julia)

---
+++ rec_int_mult/main.jl
@@ -0,0 +1,24 @@
+#
+# Recursive Integer Multiplication
+#
+# Input: two n-digit positive integers x and y
+# Output: the product x * y
+# Assumption: n is a power of 2
+#
+function go(x, y)
+  n = length(string(x)) # assume the length of x and y is the same
+  if n == 1
+    return x * y
+  end
+  m = 10 ^ div(n, 2) # middle decimal value
+  a, b = div(x, m), x % m
+  c, d = div(y, m), y % m
+  return 10^n * go(a, c) + 10^div(n, 2) * (go(a, d) + go(b, c)) + go(b, d)
+end
+
+x = 5678
+y = 1234
+println(go(x, y))
+
+# ➜  rec_int_mult git:(main) ✗ julia ./main.jl
+# 7006652
