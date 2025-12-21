1b8ca3154d749797d7cd1e7076bb4c1bf3fa143d
claytonjwong <claytonjwong@gmail.com>
2025-12-21 09:34:27 -0700

    add @testset for RecIntMult and Karatsuba

--- karatsuba/main.jl
+++ karatsuba/main.jl
@@ -5,10 +5,15 @@
 # Output: the product x * y
 # Assumption: n is a power of 2
 #
+using Test, Random
+
 function go(x, y)
-  n = min(length(string(x)), length(string(y)))
-  if n == 1
+  if x < 10 || y < 10
     return x * y
+  end
+  n = max(length(string(x)), length(string(y)))
+  if isodd(n)
+    n += 1
   end
   m = 10 ^ div(n, 2) # middle decimal value
   a, b = div(x, m), mod(x, m)
@@ -21,9 +26,20 @@ function go(x, y)
   return 10^n * ac + 10^div(n, 2) * adbc + bd
 end
 
-x = 5678
-y = 1234
-println(go(x, y))
+Random.seed!(123456789)
 
-# ➜  karatsuba git:(main) ✗ julia ./main.jl
-# 7006652
+@testset "Karatsuba Multiplication tests" begin
+    for _ in 1:100
+        n = rand((1, 2, 4, 8, 16))
+
+        lo = 10^(n-1)
+        hi = 10^n - 1
+
+        x = rand(lo:hi)
+        y = rand(lo:hi)
+
+        expect, actual = x * y, go(x, y)
+        @test expect == actual
+        println("($x x $y)\nexpect: $expect\nactual: $actual\n")
+    end
+end
--- rec_int_mult/main.jl
+++ rec_int_mult/main.jl
@@ -5,10 +5,15 @@
 # Output: the product x * y
 # Assumption: n is a power of 2
 #
+using Test, Random
+
 function go(x, y)
-  n = min(length(string(x)), length(string(y)))
-  if n == 1
+  if x < 10 || y < 10
     return x * y
+  end
+  n = max(length(string(x)), length(string(y)))
+  if isodd(n)
+    n += 1
   end
   m = 10 ^ div(n, 2) # middle decimal value
   a, b = div(x, m), mod(x, m)
@@ -20,9 +25,20 @@ function go(x, y)
   return 10^n * ac + 10^div(n, 2) * (ad + bc) + bd
 end
 
-x = 5678
-y = 1234
-println(go(x, y))
+Random.seed!(123456789)
 
-# ➜  rec_int_mult git:(main) ✗ julia ./main.jl
-# 7006652
+@testset "Recursive Integer Multiplication tests" begin
+    for _ in 1:100
+        n = rand((1, 2, 4, 8, 16))
+
+        lo = 10^(n-1)
+        hi = 10^n - 1
+
+        x = rand(lo:hi)
+        y = rand(lo:hi)
+
+        expect, actual = x * y, go(x, y)
+        @test expect == actual
+        println("($x x $y)\nexpect: $expect\nactual: $actual\n")
+    end
+end
