a88b79b3bd24c18e1d85ceccf72672cf8e297f0c
claytonjwong <claytonjwong@gmail.com>
2025-12-21 09:13:06 -0700

    minor edits -- list out each recursive call into separate variables

--- rec_int_mult/main.jl
+++ rec_int_mult/main.jl
@@ -6,14 +6,18 @@
 # Assumption: n is a power of 2
 #
 function go(x, y)
-  n = length(string(x)) # assume the length of x and y is the same
+  n = min(length(string(x)), length(string(y)))
   if n == 1
     return x * y
   end
   m = 10 ^ div(n, 2) # middle decimal value
   a, b = div(x, m), mod(x, m)
   c, d = div(y, m), mod(y, m)
-  return 10^n * go(a, c) + 10^div(n, 2) * (go(a, d) + go(b, c)) + go(b, d)
+  ac = go(a, c)
+  ad = go(a, d)
+  bc = go(b, c)
+  bd = go(b, d)
+  return 10^n * ac + 10^div(n, 2) * (ad + bc) + bd
 end
 
 x = 5678
